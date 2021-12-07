26ecbdb336a810b097bbe3693fb6c95d25d50240
JopStro <joseph.strong@live.co.uk>
2021-12-07 15:50:31 +0000

    imagine using a map for day 7, couldn't be me

--- src/DaySeven.hs
+++ src/DaySeven.hs
@@ -1,45 +1,33 @@
 -- |
 
 module DaySeven where
-import Data.Map (Map, toList, fromList, insertWith)
-import Data.List (sortBy)
+import Data.List (sort)
 
-countPositions :: [Int] -> Map Int Int
-countPositions = foldl (\m x -> insertWith (+) x 1 m) mempty
-
-findOptimalFuelp2 :: Map Int Int -> Int
-findOptimalFuelp2 m = (`calcFuel` m) $ meanKey m
+findOptimalFuelp2 :: [Int] -> Int
+findOptimalFuelp2 xs = (`calcFuel` xs) $ meanKey xs
   where
     calcFuel = calcFuelBy sumOfNats
 
-findOptimalFuelp1 :: Map Int Int -> Int
-findOptimalFuelp1 m = (`calcFuel` m) $ medianKey m
+findOptimalFuelp1 :: [Int] -> Int
+findOptimalFuelp1 xs = (`calcFuel` xs) $ medianKey xs
   where
     calcFuel = calcFuelBy id
 
-calcFuelBy :: (Int -> Int) -> Int -> Map Int Int -> Int
-calcFuelBy f x = foldl (\total (pos,freq) -> total + (f (abs (pos - x)) * freq)) 0 . toList
+calcFuelBy :: (Int -> Int) -> Int -> [Int] -> Int
+calcFuelBy f x = foldl (\total pos -> total + f (abs (pos - x))) 0
 
 -- | the sum of natural numbers
 sumOfNats :: Int -> Int
 sumOfNats n =  ((n*n) + n) `div` 2
 
-meanKey :: Map Int Int -> Int
-meanKey m = round mean
-  where
-    (amount,total) = foldr (\(k,x) (a,t) -> (x + a, (k*x) + t)) (0,0) $ toList m
-    mean = fromIntegral total / fromIntegral amount :: Double
+meanKey :: [Int] -> Int
+meanKey xs = round (fromIntegral (sum xs) / fromIntegral (length xs))
 
-medianKey :: Map Int Int -> Int
-medianKey m = index (sortBy (\(k1,_) (k2,_) -> compare k1 k2) $ toList m) $ round middle
-  where
-    middle = (/2) . fromIntegral $ foldr (\(_,x) -> (x+)) 0 $ toList m :: Double
-    index ((k,x):xs) n | n <= x    = k
-                       | otherwise = index xs (n - x)
-    index _ _ = head . map fst $ toList m
+medianKey :: [Int] -> Int
+medianKey xs = sort xs !! floor (fromIntegral (length xs) / 2 :: Double)
 
 
 examineFile :: String -> IO [Int]
 examineFile xs = do
-  positions <- countPositions . (\x -> read $ "["++x++"]") <$> readFile xs
+  positions <- (\x -> read $ "["++x++"]") <$> readFile xs
   return $ map (\f -> f positions) [findOptimalFuelp1, findOptimalFuelp2]
