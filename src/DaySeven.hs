92deede8d009ec733b902e30a02f866e5f3ecd5e
JopStro <joseph.strong@live.co.uk>
2021-12-07 16:51:36 +0000

    day 7 with vectors

--- src/DaySeven.hs
+++ src/DaySeven.hs
@@ -1,33 +1,36 @@
 -- |
 
 module DaySeven where
-import Data.List (sort)
+import Data.Vector.Unboxed (Vector, fromList, (!))
+import qualified Data.Vector.Unboxed as V
+import Statistics.Sample (mean)
+import Statistics.Function (sort)
 
-findOptimalFuelp2 :: [Int] -> Int
-findOptimalFuelp2 xs = (`calcFuel` xs) $ meanKey xs
+findOptimalFuelp2 :: Vector Int -> Int
+findOptimalFuelp2 xs = (`calcFuel` xs) $ meanPos xs
   where
     calcFuel = calcFuelBy sumOfNats
 
-findOptimalFuelp1 :: [Int] -> Int
-findOptimalFuelp1 xs = (`calcFuel` xs) $ medianKey xs
+findOptimalFuelp1 :: Vector Int -> Int
+findOptimalFuelp1 xs = (`calcFuel` xs) $ medianPos xs
   where
     calcFuel = calcFuelBy id
 
-calcFuelBy :: (Int -> Int) -> Int -> [Int] -> Int
-calcFuelBy f x = foldl (\total pos -> total + f (abs (pos - x))) 0
+calcFuelBy :: (Int -> Int) -> Int -> Vector Int -> Int
+calcFuelBy f x = V.foldl (\total pos -> total + f (abs (pos - x))) 0
 
 -- | the sum of natural numbers
 sumOfNats :: Int -> Int
 sumOfNats n =  ((n*n) + n) `div` 2
 
-meanKey :: [Int] -> Int
-meanKey xs = round (fromIntegral (sum xs) / fromIntegral (length xs))
+meanPos :: Vector Int -> Int
+meanPos = round . mean . V.map fromIntegral
 
-medianKey :: [Int] -> Int
-medianKey xs = sort xs !! floor (fromIntegral (length xs) / 2 :: Double)
+medianPos :: Vector Int -> Int
+medianPos = floor . (\v -> sort v ! floor (fromIntegral (V.length v) / 2)) . V.map fromIntegral
 
 
 examineFile :: String -> IO [Int]
 examineFile xs = do
-  positions <- (\x -> read $ "["++x++"]") <$> readFile xs
-  return $ map (\f -> f positions) [findOptimalFuelp1, findOptimalFuelp2]
+  positions <- fromList . (\x -> read $ "["++x++"]") <$> readFile xs
+  return $ map (\f -> f positions) [findOptimalFuelp1,findOptimalFuelp2]
