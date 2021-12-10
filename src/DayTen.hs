175de7cb0c11448296ea5fed5b42fa41ff0ce267
JopStro <joseph.strong@live.co.uk>
2021-12-10 13:06:17 +0000

    day 10 do both parts at once

--- src/DayTen.hs
+++ src/DayTen.hs
@@ -1,7 +1,7 @@
 -- |
 
 module DayTen where
-import Data.Maybe
+import Data.Either
 import Data.List
 import Data.Map (Map, (!), fromList)
 
@@ -10,8 +10,6 @@ type Stack = [Char]
 opening :: [Char]
 opening = "([{<"
 
-
-
 closeMap :: Map Char Char
 closeMap = fromList [ ('(', ')')
                     , ('[', ']')
@@ -19,16 +17,15 @@ closeMap = fromList [ ('(', ')')
                     , ('<', '>')
                     ]
 
-checkSyntax :: String -> Stack -> Maybe String
-checkSyntax [] xs = Just xs
+checkSyntax :: String -> Stack -> Either Char String
+checkSyntax [] xs = Right xs
 checkSyntax (x:xs) ys     | x `elem` opening = checkSyntax xs (closeMap ! x:ys)
 checkSyntax (x:xs) (y:ys) | x == y           = checkSyntax xs ys
-                          | otherwise        = Nothing
+                          | otherwise        = Left x
 
 calculateScore :: [Char] -> Int
 calculateScore = foldl (\total x -> total + (scores ! x)) 0
   where
-    scores :: Map Char Int
     scores = fromList [ (')', 3)
                       , (']', 57)
                       , ('}', 1197)
@@ -39,12 +36,13 @@ calculateCompletionScore :: [String] -> Int
 calculateCompletionScore xs = lineScores !! (ceiling (fromIntegral (length xs) / 2 :: Double) - 1)
   where
     lineScores = sort $ map (foldl (\total x -> (total * 5) + (scores ! x)) 0) xs
-    scores :: Map Char Int
     scores = fromList [ (')', 1)
                       , (']', 2)
                       , ('}', 3)
                       , ('>', 4)
                       ]
 
-validateFile :: String -> IO Int
-validateFile xs = calculateCompletionScore . mapMaybe (`checkSyntax` []) . lines <$> readFile xs
+validateFile :: String -> IO (Int,Int)
+validateFile xs = do
+  errors <- map (`checkSyntax` []) . lines <$> readFile xs
+  return (calculateScore $ lefts errors, calculateCompletionScore $ rights errors)
