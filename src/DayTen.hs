bdf5af5adcadff7bba30cf16a7bca437369aaebf
JopStro <joseph.strong@live.co.uk>
2021-12-10 14:16:23 +0000

    day 10 optimise a little

--- src/DayTen.hs
+++ src/DayTen.hs
@@ -3,46 +3,44 @@
 module DayTen where
 import Data.Either
 import Data.List
-import Data.Map (Map, (!), fromList)
 
 type Stack = [Char]
 
 opening :: [Char]
 opening = "([{<"
 
-closeMap :: Map Char Char
-closeMap = fromList [ ('(', ')')
-                    , ('[', ']')
-                    , ('{', '}')
-                    , ('<', '>')
-                    ]
+close :: Char -> Maybe Char
+close '(' = Just ')'
+close '[' = Just ']'
+close '{' = Just '}'
+close '<' = Just '>'
+close _   = Nothing
 
 checkSyntax :: String -> Stack -> Either Char String
 checkSyntax [] xs = Right xs
-checkSyntax (x:xs) ys     | x `elem` opening = checkSyntax xs (closeMap ! x:ys)
-checkSyntax (x:xs) (y:ys) | x == y           = checkSyntax xs ys
-                          | otherwise        = Left x
+checkSyntax (x:xs) stack = case close x of
+                             Just c -> checkSyntax xs (c:stack)
+                             Nothing -> if x == head stack then
+                                          checkSyntax xs $ tail stack
+                                        else
+                                          Left x
+
+score :: Char -> (Int, Integer)
+score ')' = (3,1)
+score ']' = (57,2)
+score '}' = (1197,3)
+score '>' = (25137,4)
+score _   = (0,0)
 
 calculateScore :: [Char] -> Int
-calculateScore = foldl (\total x -> total + (scores ! x)) 0
-  where
-    scores = fromList [ (')', 3)
-                      , (']', 57)
-                      , ('}', 1197)
-                      , ('>', 25137)
-                      ]
+calculateScore = foldl (\total x -> total + fst (score x)) 0
 
-calculateCompletionScore :: [String] -> Int
+calculateCompletionScore :: [String] -> Integer
 calculateCompletionScore xs = lineScores !! (ceiling (fromIntegral (length xs) / 2 :: Double) - 1)
   where
-    lineScores = sort $ map (foldl (\total x -> (total * 5) + (scores ! x)) 0) xs
-    scores = fromList [ (')', 1)
-                      , (']', 2)
-                      , ('}', 3)
-                      , ('>', 4)
-                      ]
+    lineScores = sort $ map (foldl (\total x -> (total * 5) + snd (score x)) 0) xs
 
-validateFile :: String -> IO (Int,Int)
+validateFile :: String -> IO (Int,Integer)
 validateFile xs = do
   errors <- map (`checkSyntax` []) . lines <$> readFile xs
   return (calculateScore $ lefts errors, calculateCompletionScore $ rights errors)
